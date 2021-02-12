d9e763516dfd92e69097e4aaa62321cc124c8fea
Philipp Wagner <pwag86@googlemail.com>
2021-02-12 08:30:41 +0100

    Issue #17 Initial Test

---
+++ JSqlServerBulkInsert/src/test/java/de/bytefish/jsqlserverbulkinsert/test/issues/Issue17Test.java
@@ -0,0 +1,88 @@
+package de.bytefish.jsqlserverbulkinsert.test.issues;
+
+import de.bytefish.jsqlserverbulkinsert.SqlServerBulkInsert;
+import de.bytefish.jsqlserverbulkinsert.test.base.TransactionalTestBase;
+import de.bytefish.jsqlserverbulkinsert.test.integration.PersonMapping;
+import de.bytefish.jsqlserverbulkinsert.test.model.Person;
+import de.bytefish.jsqlserverbulkinsert.test.utils.MeasurementUtils;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * https://github.com/JSqlServerBulkInsert/JSqlServerBulkInsert/issues/17
+ */
+public class Issue17Test extends TransactionalTestBase {
+
+    @Override
+    protected void onSetUpInTransaction() throws Exception {
+        createTable();
+    }
+
+    @Test
+    public void bulkInsertPersonDataTest() throws SQLException {
+        // The Number of Entities to insert:
+        int numEntities = 1000000;
+        // Create a large list of Persons:
+        List<Person> persons = getPersonList(numEntities);
+        // Create the Mapping:
+        PersonMapping mapping = new PersonMapping();
+        // Create the Bulk Inserter:
+        SqlServerBulkInsert<Person> bulkInsert = new SqlServerBulkInsert<>(mapping);
+        // Measure the Bulk Insert time:
+        MeasurementUtils.MeasureElapsedTime("Bulk Insert 1000000 Entities", () -> {
+            // Now save all entities of a given stream:
+            bulkInsert.saveAll(connection, persons.stream());
+        });
+        // And assert all have been written to the database:
+        Assert.assertEquals(numEntities, getRowCount());
+    }
+
+    private List<Person> getPersonList(int numPersons) {
+        List<Person> persons = new ArrayList<>();
+
+        for (int pos = 0; pos < numPersons; pos++) {
+            Person p = new Person();
+
+            p.setFirstName("Philipp");
+            p.setLastName("Wagner");
+            p.setBirthDate(LocalDate.of(1986, 5, 12));
+
+            persons.add(p);
+        }
+
+        return persons;
+    }
+
+    private boolean createTable() throws SQLException {
+
+        String sqlStatement = "CREATE TABLE [dbo].[UnitTest]\n" +
+                "            (\n" +
+                "                FirstName NVARCHAR(255),\n" +
+                "                LastName NVARCHAR(255),\n" +
+                "                BirthDate DATE\n" +
+                "            );";
+
+        Statement statement = connection.createStatement();
+
+        return statement.execute(sqlStatement);
+    }
+
+    private int getRowCount() throws SQLException {
+
+        Statement s = connection.createStatement();
+
+        ResultSet r = s.executeQuery("SELECT COUNT(*) AS total FROM [dbo].[UnitTest];");
+        r.next();
+        int count = r.getInt("total");
+        r.close();
+
+        return count;
+    }
+}
